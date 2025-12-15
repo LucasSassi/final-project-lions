036c513d70650bacb59b941a6b325e39e9296582
Lucas Sassi de Souza <sassi.lucas17@gmail.com>
2025-12-15 18:12:51 -0300

    feat: add CORS support

--- backend/src/app.js
+++ backend/src/app.js
@@ -6,6 +6,18 @@ import AppError from './utils/app-error.js';
 
 const app = express();
 
+// CORS middleware
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+  
+  if (req.method === 'OPTIONS') {
+    return res.sendStatus(200);
+  }
+  next();
+});
+
 // Middlewares globais
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
